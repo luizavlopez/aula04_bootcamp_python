885466c43125a827b53f13d9acf6e0f0305dd3f1
Luiza Vettorazzi <luizavlopez@gmail.com>
2025-04-06 18:24:38 -0300

    adição do ex 02

---
+++ ex02.py
@@ -0,0 +1,10 @@
+# Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+
+linguagens: list = ["Python", "Java", "C++", "JavaScript"]
+
+print(linguagens)
+
+linguagens.remove("C++")
+linguagens.append("Ruby")
+
+print(linguagens)
