8bbf754a776a726a634b724bd44511e557a15702
Luiza Vettorazzi <luizavlopez@gmail.com>
2025-04-06 18:24:24 -0300

    adição do ex 01

---
+++ ex01.py
@@ -0,0 +1,7 @@
+# Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+lista_de_numeros: list = []
+
+lista_de_numeros.extend(range(1,11))
+
+for numero in lista_de_numeros:
+    print(numero)
