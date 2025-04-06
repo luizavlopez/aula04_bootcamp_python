d568c0acf9a0156f19ec26430762076f7618daa1
Luiza Vettorazzi <luizavlopez@gmail.com>
2025-04-06 18:24:49 -0300

    adição do ex 03

---
+++ ex03.py
@@ -0,0 +1,25 @@
+# Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. 
+# Imprima cada informação.
+
+livros: dict = {
+    "livro_01": {
+        "título": "Cem Anos de Solidão",
+        "autor": "Gabriel García Márquez",
+        "ano": "1967"},
+    
+    "livro_02": {
+        "título": "1984",
+        "autor": "George Orwell",
+        "ano": "1949"},
+    
+    "livro_03": {
+        "título": "Dom Casmurro",
+        "autor": "Machado de Assis",
+        "ano": "1899"}
+    }
+
+for chave, dados in livros.items():
+    print(f"Título: {dados['título']}")
+    print(f"Autor: {dados['autor']}")
+    print(f"Ano: {dados['ano']}")
+    print()
